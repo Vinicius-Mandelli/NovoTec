3271b0230ea93b55c919fbced46671207f63c07d
Vinicius Mandelli <viniciusmandelliqueiroz@gmail.com>
2023-08-22 09:41:07 -0300

    maçã

---
+++ 2208/soma.js
@@ -0,0 +1,11 @@
+// equação de primeiro grau
+
+let preco = 5.50
+let qnt_maca_comprada = 3
+let valor_pagar = preco * qnt_maca_comprada
+
+console.log("você deve", valor_pagar, "a pagar")
+console.log("o preço unitario das maças é:", preco)
+
+
+
