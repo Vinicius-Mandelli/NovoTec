7cf5ff4926029a9bb820cc481e1fb683e6a7015a
Vinicius Mandelli <viniciusmandelliqueiroz@gmail.com>
2023-08-22 11:21:39 -0300

    novos arquivos

---
+++ 2208/jsjogos.js
@@ -0,0 +1,10 @@
+let inicial = 150;
+let bonus = 80;
+let perdidos = 30;
+
+var final = inicial + bonus - perdidos;
+
+console.log("parabéns sua pontuação final é",final);
+console.log("você perdeu", perdidos);
+console.log("a pontuação inicial é", inicial)
+ console.log("a pontuação bonus da fase é", bonus)
---
+++ 2208/jsproduto.js
@@ -0,0 +1,10 @@
+let inicial = 150;
+let bonus = 80;
+let perdidos = 30;
+
+var final = inicial + bonus - perdidos;
+
+console.log("a quantidade inicial é", inicial)
+console.log("a quantidade comprada é", bonus)
+console.log("você vendeu", perdidos, "produtos");
+console.log("o número de produtos final é",final);
